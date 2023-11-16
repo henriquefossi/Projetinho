bc82de9da5895b9bc81cd1de7ee9048232fd86a8
Henrique Fossi <hffeliu@gmail.com>
2023-11-16 20:29:13 -0300

    projeto

---
+++ socialnetworks/serializer.py
@@ -0,0 +1,8 @@
+from .models import Socialnetwork
+from rest_framework import serializers
+
+class SocialnetworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Socialnetwork
+        fields = '__all__'
+
